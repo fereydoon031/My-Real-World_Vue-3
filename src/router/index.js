5bc1b093e729debc0a74264f7831452f73ca3ac3
Fereydoon <ferydoon.jafari@gmail.com>
2022-03-29 18:15:50 +0430

    feat: Redirect & alias added

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import About from "../views/AboutView.vue";
 import EventLayout from "../views/event/EventLayout.vue";
 import EventDetails from "../views/event/EventDetails.vue";
 import EventRegister from "../views/event/EventRegister.vue";
@@ -13,7 +14,7 @@ const routes = [
     props: (route) => ({ page: parseInt(route.query.page) || 1 }),
   },
   {
-    path: "/event/:id",
+    path: "/events/:id",
     name: "EventLayout",
     props: true,
     component: EventLayout,
@@ -36,13 +37,19 @@ const routes = [
     ],
   },
   {
+    path: "/event/:afterEvent(.*)",
+    redirect: (to) => {
+      return { path: "/events/" + to.params.afterEvent };
+    },
+  },
+  {
+    path: "/about-us",
+    name: "About",
+    component: About,
+  },
+  {
     path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    redirect: "/about-us",
   },
 ];
 
